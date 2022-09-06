27286cdb46a662ce53441b414c8c6e5fd089b202
gaosen <0x5e@sina.cn>
2022-09-06 15:44:57 +0800

    update index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
+const settings_1 = require("./settings");
 const platform_1 = require("./platform");
 module.exports = (api) => {
-    api.registerPlatform('YindlPlatform', platform_1.YindlPlatform);
+    api.registerPlatform(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, platform_1.YindlPlatform);
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { API } from 'homebridge';
 
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { YindlPlatform } from './platform';
 
 export = (api: API) => {
-  api.registerPlatform('YindlPlatform', YindlPlatform);
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, YindlPlatform);
 };
