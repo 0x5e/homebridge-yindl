76bb73d3cd13ba1940ed458fea9904f2030a7590
gaosen <0x5e@sina.cn>
2022-10-12 13:25:25 +0800

    Update project file from template

--- src/platform.ts
+++ src/platform.ts
@@ -4,13 +4,19 @@ import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { YindlClient } from './client';
 import { YindlLightbulbPlatformAccessory } from './light';
 
+/**
+ * HomebridgePlatform
+ * This class is the main constructor for your plugin, this is where you should
+ * parse the user config and discover/register accessories with Homebridge.
+ */
 export class YindlPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
+  // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
 
-  public client: YindlClient;
+  public client: YindlClient | undefined;
   public lights: YindlLightbulbPlatformAccessory[] = [];
 
   constructor(
@@ -20,17 +26,39 @@ export class YindlPlatform implements DynamicPlatformPlugin {
   ) {
     this.log.debug('Finished initializing platform:', this.config.name);
 
-    this.client = new YindlClient(config.host, config.port);
-    this.client.on('loaded', this.loaded.bind(this));
-    this.client.on('event', this.event.bind(this));
-
-    api.on('didFinishLaunching', async () => {
-      this.client.start();
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      // run the method to discover / register your devices as accessories
+      this.discoverDevices();
     });
   }
 
-  configureAccessory(accessory) {
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+
+    // add the restored accessory to the accessories cache so we can track if it has already been registered
     this.accessories.push(accessory);
+  }
+
+  /**
+   * This is an example method showing how to register discovered accessories.
+   * Accessories must only be registered once, previously created accessories
+   * must not be registered again to prevent "duplicate UUID" errors.
+   */
+  discoverDevices() {
+    const client = new YindlClient(this.config.host, this.config.port);
+    client.on('loaded', this.loaded.bind(this));
+    client.on('event', this.event.bind(this));
+    client.start();
+    this.client = client;
   }
 
   loaded() {
