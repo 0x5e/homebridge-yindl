0780a9633b7e57e0adda4a91eed68761f7626a39
gaosen <0x5e@sina.cn>
2022-10-12 13:25:32 +0800

    remove dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const settings_1 = require("./settings");
-const platform_1 = require("./platform");
-module.exports = (api) => {
-    api.registerPlatform(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, platform_1.YindlPlatform);
-};
-//# sourceMappingURL=index.js.map
--- dist/settings.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
-export declare const PLATFORM_NAME = "YindlPlatform";
-/**
- * This must match the name of your plugin as defined the package.json
- */
-export declare const PLUGIN_NAME = "homebridge-yindl";
-//# sourceMappingURL=settings.d.ts.map
